6de251c2410f2f36d94d53598249404123594e4a
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-04-18 16:28:30 -0400

    hw done

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,30 @@
-function checkForSpam(message) {
-  const updatedMessage = message.toLowerCase();
-  return updatedMessage.includes('spam') || updatedMessage.includes('sale');
+function filterArray(numbers, value) {
+  let newArray = [];
+  for (let num of numbers) {
+    if (num > value) {
+      newArray.push(num);
+    }
+  }
+  return newArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// Вирішила зробити з тернарним оператором
+function filterArray(numbers, value) {
+  let newArray = [];
+  for (let num of numbers) {
+    num > value ? newArray.push(num) : null;
+  }
+  return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
